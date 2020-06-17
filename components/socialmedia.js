c03e9456f265f32467e12b2820ae8934f8ca5f6c
Pauline Narvas <pauline@paulinenarvas.com>
2020-06-17 13:25:32 +0100

    fix social media css

--- components/socialmedia.js
+++ components/socialmedia.js
@@ -9,7 +9,7 @@ const Twitter = () => (
     xmlns="http://www.w3.org/2000/svg"
     viewBox="0 0 512 512"
   >
-    <style jsx>
+    <style>
       {`
         .socialmedia {
           width: 30px;
