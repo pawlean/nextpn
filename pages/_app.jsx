f58b6d3d379e3d5550489fc7afa6dd596d0d7a13
Pauline Narvas <pauline@paulinenarvas.com>
2020-06-17 22:14:50 +0100

    Add GH icon in header

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,11 +1,18 @@
-import { Twitter, LinkedIn, Instagram, Email } from "../components/socialmedia";
+import {
+  Twitter,
+  LinkedIn,
+  Instagram,
+  Email,
+  GitHub,
+} from "../components/socialmedia";
 import { VerticleButton as ScrollUpButton } from "react-scroll-up-button";
+
 function MyApp({ Component, pageProps }) {
   return (
     <div>
       <Component {...pageProps} />
       <ScrollUpButton />
-      <footer>
+      <header>
         <div className="social">
           <a href="http://twitter.com/paulienuh" target="_blank">
             <Twitter />
@@ -18,11 +25,14 @@ function MyApp({ Component, pageProps }) {
           <a href="http://instagram.com/paw.lean" target="_blank">
             <Instagram />
           </a>
+          <a href="http://github.com/pawlean" target="_blank">
+            <GitHub />
+          </a>
           <a href="mailto:pauline@paulinenarvas.com" target="_blank">
             <Email />
           </a>
         </div>
-      </footer>
+      </header>
 
       <style jsx>
         {`
@@ -30,8 +40,7 @@ function MyApp({ Component, pageProps }) {
             float: right;
             margin: 10px;
           }
-
-          footer {
+          header {
             width: 100%;
             height: 50px;
             background-color: #a47da4;
@@ -40,7 +49,7 @@ function MyApp({ Component, pageProps }) {
             top: 0;
             width: 100%;
           }
-          footer a {
+          header a {
             color: white;
           }
         `}
