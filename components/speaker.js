46e10645cf18ed74e683f162d25341f969152ba0
Pauline Narvas <pauline@paulinenarvas.com>
2020-06-20 22:07:40 +0100

    Add lazy loading for youtube

--- components/speaker.js
+++ components/speaker.js
@@ -14,6 +14,7 @@ function Speaker() {
           title="Pauline's Speaking Gigs"
           width="100%"
           height="450"
+          srcdoc="<style>*{padding:0;margin:0;overflow:hidden}html,body{height:100%}img,span{position:absolute;width:100%;top:0;bottom:0;margin:auto}span{height:1.5em;text-align:center;font:48px/1.5 sans-serif;color:white;text-shadow:0 0 0.5em black}</style><a href=https://www.youtube.com/embed/videoseries?list=PLkR4A7sk9JzbANRRuAwlTODpAlyDJkpqG><img src=images/main/Burnout.png alt='Pauline's Speaking Gigs around the world'><span>▶</span></a>"
           src="https://www.youtube.com/embed/videoseries?list=PLkR4A7sk9JzbANRRuAwlTODpAlyDJkpqG"
           frameBorder="0"
           allow="autoplay; encrypted-media"
