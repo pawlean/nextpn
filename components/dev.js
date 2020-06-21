3700831be121833549f628f9a91170cadddaafda
Pauline Narvas <pauline@paulinenarvas.com>
2020-06-21 17:22:44 +0100

    fix typo

--- components/dev.js
+++ components/dev.js
@@ -5,7 +5,7 @@ import { LazyLoadImage } from "react-lazy-load-image-component";
 function Dev() {
   return (
     <div className={styles.greycontainer}>
-      <h1>💻 Enthuastic Techie</h1>
+      <h1>💻 Enthusiastic Techie</h1>
       <p>
         I began learning to code for fun when I was 8 years old.{" "}
         <a
