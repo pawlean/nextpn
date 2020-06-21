28dc9174292f2b4deab799186a6a08e639d6ec2c
Pauline Narvas <pauline@paulinenarvas.com>
2020-06-21 11:00:35 +0100

    Add videowrapper css to module

--- components/speaker.js
+++ components/speaker.js
@@ -9,12 +9,36 @@ function Speaker() {
         I began speaking at events in 2017, my goal was to inspire others to
         reach for the stars in their career and beyond. 🚀
       </p>
-      <div className={styles.videoWrapper}>
+      <div className="videoWrapper">
         <iframe
           title="Pauline's Speaking Gigs"
           width="100%"
           height="450"
-          srcDoc="<style>*{padding:0;margin:0;overflow:hidden}html,body{height:100%}img,span{position:absolute;width:100%;top:0;bottom:0;margin:auto}span{height:1.5em;text-align:center;font:48px/1.5 sans-serif;color:white;text-shadow:0 0 0.5em black}</style><a href=https://www.youtube.com/embed/videoseries?list=PLkR4A7sk9JzbANRRuAwlTODpAlyDJkpqG><img src=images/main/Burnout.png alt='Pauline's Speaking Gigs around the world'><span>▶</span></a>"
+          srcDoc="<style>
+          * {
+            padding:0;
+            margin:0;
+            overflow:hidden
+          }
+          img,span {
+            position:absolute;
+            width:100%;
+            top:0;
+            bottom:0;
+            margin:auto
+          }
+          span {
+            height:1.5em;
+            text-align:center;
+            font:48px/1.5 sans-serif;
+            color: white;
+            text-shadow:0 0 0.5em black
+          }
+          </style>
+          <a href=https://www.youtube.com/embed/videoseries?list=PLkR4A7sk9JzbANRRuAwlTODpAlyDJkpqG>
+          <img src=images/main/Burnout.png alt='Pauline's Speaking Gigs around the world'>
+          <span>▶</span>
+          </a>"
           src="https://www.youtube.com/embed/videoseries?list=PLkR4A7sk9JzbANRRuAwlTODpAlyDJkpqG"
           frameBorder="0"
           allow="autoplay; encrypted-media"
@@ -22,6 +46,14 @@ function Speaker() {
         ></iframe>
       </div>
       <Talks />
+      <style jsx>
+        {`
+          .videoWrapper {
+            max-width: 560px;
+            margin: 0 auto;
+          }
+        `}
+      </style>
     </div>
   );
 }
