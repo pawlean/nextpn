068b056c121016919e04433128517b7009f969ac
Pauline Narvas <pauline@paulinenarvas.com>
2020-06-21 12:34:24 +0100

    Create custom 404 page

---
+++ pages/404.js
@@ -0,0 +1,31 @@
+export default function Custom404() {
+  return (
+    <div>
+      <h1>
+        Oops - my bad! 🤷🏻‍♀️ Return to{" "}
+        <a href="https://PaulineNarvas.com">PaulineNarvas.com</a>
+      </h1>
+      <style jsx>
+        {`
+          div {
+            padding-top: 6rem;
+          }
+          h1 {
+            font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+              Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+              sans-serif;
+            text-align: center;
+          }
+          a {
+            color: #a47da4;
+            border-bottom: 1px dotted #a47da4;
+            text-decoration: none;
+          }
+          a:hover {
+            border-bottom: 1px solid #a47da4;
+          }
+        `}
+      </style>
+    </div>
+  );
+}
