fcce8b159afca7f77e53172b63a6f3f7833ee6f6
Pauline Narvas <pauline@paulinenarvas.com>
2020-06-21 12:28:23 +0100

    add next.config.js

--- next.config.js
+++ next.config.js
@@ -5,7 +5,24 @@ module.exports = withPlugins([
   [
     optimizedImages,
     {
+      inlineImageLimit: 8192,
+      imagesFolder: "images",
+      imagesName: "[name]-[hash].[ext]",
+      handleImages: ["jpeg", "png", "svg", "webp", "gif"],
+      removeOriginalExtension: false,
+      optimizeImages: true,
       optimizeImagesInDev: true,
+      mozjpeg: {
+        quality: 80,
+      },
+      optipng: {
+        optimizationLevel: 3,
+      },
+      pngquant: false,
+      webp: {
+        preset: "default",
+        quality: 75,
+      },
     },
   ],
 ]);
