f4e4886b9085937e54b359e9cf9e7481d665cc40
Pauline P. Narvas (Paw) <pauline@paulinenarvas.com>
2021-06-01 14:13:14 +0000

    Update on Community

--- components/community.js
+++ components/community.js
@@ -6,13 +6,17 @@ function Community() {
     <div className={styles.greycontainer}>
       <h1>🛠 Community Builder & Diversity Advocate</h1>
       <p>
-        Passionate about creating a better environment for future engineers, I
-        actively work with communities to help bridge the inequalities in the
-        tech industry, focusing speficially on females and people of colour
-        representation. 
-        
-        </p>
-        <p><strong>Women in Tech - Code First: Girls & Diversity work</strong><br/>For over 4 years, I've proudly worked with{" "}
+        Community is <em>everything</em>. I've been involved in several
+        different communities over the years with the goal of connecting with
+        others and creating a better environment for engineers. With diversity
+        and inclusion at the core of my community work, I have actively worked
+        with groups to help bridge the inequalities in the tech industry,
+        focusing speficially on females and people of colour representation.
+      </p>
+      <p>
+        <strong>Women in Tech - Code First: Girls & Diversity work</strong>
+        <br />
+        For over 5 years, I've proudly worked with{" "}
         <a
           href="http://twitter.com/codefirstgirls"
           target="_blank"
@@ -26,8 +30,7 @@ function Community() {
             className="external"
           />{" "}
         </a>
-        to deliver coding community courses for young women.{" "}
-        I have also been
+        to deliver coding community courses for young women. I have also been
         involved in the{" "}
         <a
           href="https://www.codefirstgirls.org.uk/bt--cfg-digital-intensive.html"
@@ -56,15 +59,16 @@ function Community() {
             className="external"
           />
         </a>{" "}
-        as a mentor to upskill young women into joining the industry.
+        as a mentor to upskill young women into joining the industry. Currently,
+        I'm one of the CFG Brand Ambassadors.
       </p>
       <p>
-        In addition, I was part of the Gender Equality Network (GEN) at BT
-        where I regularly push initiatives at our Leeds base. 
+        In addition, I was part of the Gender Equality Network (GEN) at BT where
+        I regularly push initiatives at our Leeds base.
       </p>
-      <p><strong>Building communities - student groups</strong><br/>
-        With diversity and inclusion at the core of my community work, I
-        pioneered the{" "}
+      <p>
+        <strong>Building communities - student groups</strong>
+        <br />I pioneered the{" "}
         <a
           href="https://www.facebook.com/groups/1438086499595347/about/"
           target="_blank"
@@ -78,7 +82,7 @@ function Community() {
             className="external"
           />
         </a>{" "}
-        in Sheffield. Also, as part of the{" "}
+        at the University of Sheffield. Also, as part of the{" "}
         <a
           href="http://facebook.com/medtechsheffield"
           target="_blank"
@@ -94,15 +98,22 @@ function Community() {
         </a>{" "}
         committee, I led in bringing students from different backgrounds
         together to events, workshops and hackathons.
-        <br/><br/>
-        <img src="/images/CommunityAWS.png"  alt="AWS" width="100%"/>
+        <br />
+        <br />
+        <img src="/images/CommunityAWS.png" alt="AWS" width="100%" />
       </p>
-      
-      <p><strong>Amazon Web Services (AWS) - Community Builders Program</strong><br/>I became a AWS Community Builder in 2021. As part of the programme,
-        I'll be sharing my knowledge through content on my blog and YouTube Channel, helping others in their own journey getting into AWS. 
-        </p>
 
-        <p><strong>Ladies in DevOps</strong><br/>I founded the  <a
+      <p>
+        <strong>Amazon Web Services (AWS) - Community Builders Program</strong>
+        <br />I became a AWS Community Builder in 2021. As part of the
+        programme, I'll be sharing my knowledge through content on my blog and
+        YouTube Channel, helping others in their own journey getting into AWS.
+      </p>
+
+      <p>
+        <strong>Ladies in DevOps</strong>
+        <br />I founded the{" "}
+        <a
           href="https://ladiesindevops.com/"
           target="_blank"
           aria-describedby="new-window-1"
@@ -114,8 +125,12 @@ function Community() {
             alt="opens in new window"
             className="external"
           />
-        </a> community in April 2021. It's a community geared for women in the DevOps/SRE/Cloud/Infra space. It’s a safe space for them to connect and network with others and bring about visibility of the exciting roles in the industry.
-        </p>
+        </a>{" "}
+        community in April 2021. It's a community geared for women in the
+        DevOps/SRE/Cloud/Infra space. It’s a safe space for them to connect and
+        network with others and bring about visibility of the exciting roles in
+        the industry.
+      </p>
 
       <h2>Projects</h2>
       <div className={styles.features}>
@@ -123,14 +138,12 @@ function Community() {
           {
             title: "Computer Weekly",
             image: "images/projects/Pay-It-Forward.png",
-            link:
-              "https://www.computerweekly.com/video/Amali-de-Alwis-most-influential-woman-in-UK-IT-2018-winners-speech",
+            link: "https://www.computerweekly.com/video/Amali-de-Alwis-most-influential-woman-in-UK-IT-2018-winners-speech",
           },
           {
             title: "Online Community Engagement",
             image: "images/projects/CFG1.png",
-            link:
-              "https://twitter.com/search?f=tweets&vertical=default&q=shefcodefirst&src=typd",
+            link: "https://twitter.com/search?f=tweets&vertical=default&q=shefcodefirst&src=typd",
           },
           {
             title: "Be Bold For Change x #shefcodefirst",
@@ -238,7 +251,11 @@ function Community() {
             </a>
           </li>
           <li>
-            <strong>April 2021:</strong> On launch day of Ladies in DevOps, we had 150 community members join organically through social media platforms. We now have a growing community of over <strong>300+ members</strong> and some exciting partnerships and sponsorships to help women advance in their careers!
+            <strong>April 2021:</strong> On launch day of Ladies in DevOps, we
+            had 150 community members join organically through social media
+            platforms. We now have a growing community of over{" "}
+            <strong>450+ members</strong> and some exciting partnerships and
+            sponsorships to help women advance in their careers!
           </li>
         </ul>
       </div>
