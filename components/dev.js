27aa6059b73bdeff0fe38f1252ee6d9de4981d76
Pauline P. Narvas (Paw) <pauline@paulinenarvas.com>
2021-07-03 13:06:16 +0000

    New job!

--- components/dev.js
+++ components/dev.js
@@ -22,9 +22,148 @@ function Dev() {
           />
         </a>{" "}
         which carries on to this day - continuing to bring my ideas to life by
-        playing around with pixels on the internet. Taking my love for
-        technology and learning further, I took to the Clouds and became AWS
-        Certified as a{" "}
+        playing around with pixels on the internet.{" "}
+      </p>
+
+      <p>
+        I've been working in tech for over 3 years with my first role was part
+        of a graduate scheme. I had the opportunity to do four 6 month rotations
+        across the different delivery teams within BT Consumer Digital
+        Engineering. Read about the start of
+        <a
+          href="https://www.linkedin.com/pulse/week-1-start-my-digital-development-operations-pauline-p-narvas/"
+          target="_blank"
+          aria-describedby="new-window-1"
+          rel="noopener"
+        >
+          {" "}
+          my Graduate Development{" "}
+          <img
+            src="/images/main/external.svg"
+            alt="opens in new window"
+            className="external"
+          />
+        </a>{" "}
+        You can also find detailed reflections of my time on the BT scheme{" "}
+        <a
+          href="https://pawlean.com/category/self-development/bt-graduate-scheme/"
+          target="_blank"
+          aria-describedby="new-window-1"
+          rel="noopener"
+        >
+          on my blog{" "}
+          <img
+            src="/images/main/external.svg"
+            alt="opens in new window"
+            className="external"
+          />
+        </a>
+        .{" "}
+      </p>
+      <h2>BT projects & features</h2>
+      <div className={styles.features}>
+        {[
+          {
+            title: "Beyond Limits Campaign",
+            image: "images/projects/BeyondLimits.png",
+            link: "https://www.youtube.com/watch?v=opxR4XRMDEw",
+          },
+          {
+            title: "BT Hack Day",
+            image: "images/projects/BTHACKDAY.png",
+            link: "https://www.youtube.com/watch?v=HE23blo7udA&t=11s",
+          },
+          {
+            title: "Campus Champions",
+            image: "images/projects/CampusChampions.png",
+            link: "https://www.youtube.com/watch?v=WrdQGjIAEHM",
+          },
+          {
+            title: "Graduate Scheme LinkedIn Video",
+            image: "images/projects/GradSchemeFeature.png",
+            link: "https://www.linkedin.com/posts/bt_technology-activity-6610814119073329153-xoBM",
+          },
+          {
+            title: "International Women's Day 2020",
+            image: "images/projects/BTIWD.png",
+            link: "https://www.linkedin.com/posts/pnarvas_internationalwomenday2020-iwd2020-womenintech-activity-6643091239837274112-01UB",
+          },
+          {
+            title: "HackSheffield 4.0 (Sponsored by BT)",
+            image: "images/projects/HS4.jpg",
+            link: "https://youtu.be/0lEVMB8YCyk",
+          },
+          {
+            title: "BT Early Careers: 12 Grads of Christmas",
+            image: "images/projects/Xmas.png",
+            link: "https://www.facebook.com/BTEarlyCareers/videos/2199772790278680/",
+          },
+          {
+            title: "BT Early Careers: Volunteering Week",
+            image: "images/projects/Volunteering.png",
+            link: "https://en-gb.facebook.com/BTEarlyCareers/photos/pb.97945334270.-2207520000../10158356243734271/?type=3&theater",
+          },
+          {
+            title: "BT Early Careers: Working From Home",
+            image: "images/projects/WFH.png",
+            link: "https://en-gb.facebook.com/BTEarlyCareers/videos/vb.97945334270/1112813405755270/?type=2&theater",
+          },
+          {
+            title: "ELEVATE Programme Careers Talk",
+            image: "images/projects/ELEVATE.png",
+            link: "https://www.youtube.com/watch?v=eA2HdTKD764",
+          },
+        ].map((feature) => (
+          <a
+            href={feature.link}
+            target="_blank"
+            aria-describedby="new-window-1"
+            rel="noopener"
+            key={feature.link}
+          >
+            <div className={styles.feature}>
+              <LazyLoadImage src={feature.image} alt={feature.title} />
+              <p>{feature.title}</p>
+            </div>
+          </a>
+        ))}
+      </div>
+      <p>
+        After the scheme, I became a Site Reliability Engineer where I
+        contributed to building Digital Engineering's platform of the future as
+        well as{" "}
+        <a
+          href="https://www.youtube.com/watch?v=r3b3Z4ieNrc"
+          target="_blank"
+          aria-describedby="new-window-1"
+          rel="noopener"
+        >
+          helped drive the newly formed SRE Team.{" "}
+          <img
+            src="/images/main/external.svg"
+            alt="opens in new window"
+            className="external"
+          />
+        </a>
+        <a
+          href="https://pawlean.com/2020/12/17/leaving-my-first-tech-job"
+          target="_blank"
+          aria-describedby="new-window-1"
+          rel="noopener"
+        >
+          I was ready for my next challenge{" "}
+          <img
+            src="/images/main/external.svg"
+            alt="opens in new window"
+            className="external"
+          />
+        </a>{" "}
+        and joined Sky Betting & Gaming as a DevOps Engineer.
+      </p>
+      <p>
+        {" "}
+        Taking my love for technology and learning further, I took to the Clouds
+        and became AWS Certified as a{" "}
         <a
           href="https://www.youracclaim.com/badges/d0028cfc-94d0-4eea-a0d8-ae3b4c74d122?source=linked_in_profile"
           target="_blank"
@@ -52,7 +191,10 @@ function Dev() {
             className="external"
           />
         </a>{" "}
-        (December 2020) I am familiar with:
+        (December 2020)
+      </p>
+      <h2>Technologies that I'm familiar with</h2>
+      <p>
         <ul className={styles.skills}>
           {[
             "HTML",
@@ -373,6 +515,7 @@ function Dev() {
           </tr>
         </table>
       </div>
+
       <style jsx>
         {`
           .previouswork {
--- components/digieng.js
+++ components/digieng.js
@@ -4,152 +4,32 @@ import { LazyLoadImage } from "react-lazy-load-image-component";
 function DigiEng() {
   return (
     <div className={styles.whitecontainer}>
-      <h1>🧩 DevOps Engineer</h1>
+      <h1>🍭 Senior Community Engineer </h1>
       <p>
-        <strong>Getting into Tech</strong>
-        <br />
-        In September 2018, I joined BT's exciting 2 year Digital Engineering
-        graduate scheme. Throughout the 2 years, I had the opportunity to do
-        four 6 month rotations across the different delivery teams within BT
-        Consumer Digital Engineering. Read about the start of
-        <a
-          href="https://www.linkedin.com/pulse/week-1-start-my-digital-development-operations-pauline-p-narvas/"
-          target="_blank"
-          aria-describedby="new-window-1"
-          rel="noopener"
-        >
-          {" "}
-          my Graduate Development{" "}
-          <img
-            src="/images/main/external.svg"
-            alt="opens in new window"
-            className="external"
-          />
-        </a>
-        . You can also find detailed reflections of my time on the BT scheme{" "}
-        <a
-          href="https://pawlean.com/category/self-development/bt-graduate-scheme/"
-          target="_blank"
-          aria-describedby="new-window-1"
-          rel="noopener"
-        >
-          on my blog{" "}
-          <img
-            src="/images/main/external.svg"
-            alt="opens in new window"
-            className="external"
-          />
-        </a>
-        .
+        I’m part of the Developer & Community Success team over at Gitpod,
+        currently leading community engagement and development.
       </p>
-      <h2>Projects</h2>
-      <div className={styles.features}>
-        {[
-          {
-            title: "Beyond Limits Campaign",
-            image: "images/projects/BeyondLimits.png",
-            link: "https://www.youtube.com/watch?v=opxR4XRMDEw",
-          },
-          {
-            title: "BT Hack Day",
-            image: "images/projects/BTHACKDAY.png",
-            link: "https://www.youtube.com/watch?v=HE23blo7udA&t=11s",
-          },
-          {
-            title: "Campus Champions",
-            image: "images/projects/CampusChampions.png",
-            link: "https://www.youtube.com/watch?v=WrdQGjIAEHM",
-          },
-          {
-            title: "Graduate Scheme LinkedIn Video",
-            image: "images/projects/GradSchemeFeature.png",
-            link:
-              "https://www.linkedin.com/posts/bt_technology-activity-6610814119073329153-xoBM",
-          },
-          {
-            title: "International Women's Day 2020",
-            image: "images/projects/BTIWD.png",
-            link:
-              "https://www.linkedin.com/posts/pnarvas_internationalwomenday2020-iwd2020-womenintech-activity-6643091239837274112-01UB",
-          },
-          {
-            title: "HackSheffield 4.0 (Sponsored by BT)",
-            image: "images/projects/HS4.jpg",
-            link: "https://youtu.be/0lEVMB8YCyk",
-          },
-          {
-            title: "BT Early Careers: 12 Grads of Christmas",
-            image: "images/projects/Xmas.png",
-            link:
-              "https://www.facebook.com/BTEarlyCareers/videos/2199772790278680/",
-          },
-          {
-            title: "BT Early Careers: Volunteering Week",
-            image: "images/projects/Volunteering.png",
-            link:
-              "https://en-gb.facebook.com/BTEarlyCareers/photos/pb.97945334270.-2207520000../10158356243734271/?type=3&theater",
-          },
-          {
-            title: "BT Early Careers: Working From Home",
-            image: "images/projects/WFH.png",
-            link:
-              "https://en-gb.facebook.com/BTEarlyCareers/videos/vb.97945334270/1112813405755270/?type=2&theater",
-          },
-          {
-            title: "ELEVATE Programme Careers Talk",
-            image: "images/projects/ELEVATE.png",
-            link: "https://www.youtube.com/watch?v=eA2HdTKD764",
-          },
-        ].map((feature) => (
-          <a
-            href={feature.link}
-            target="_blank"
-            aria-describedby="new-window-1"
-            rel="noopener"
-            key={feature.link}
-          >
-            <div className={styles.feature}>
-              <LazyLoadImage src={feature.image} alt={feature.title} />
-              <p>{feature.title}</p>
-            </div>
-          </a>
-        ))}
-      </div>
       <p>
-        <strong>Post-graduate scheme</strong>
-        <br />I rolled off the scheme as a Site Reliability Engineer where I
-        contributed to building Digital Engineering's platform of the future as
-        well as{" "}
+        <img src="images/Frame_14.svg" />
+      </p>
+      <p>
         <a
-          href="https://www.youtube.com/watch?v=r3b3Z4ieNrc"
+          href="https://gitpod.io"
           target="_blank"
           aria-describedby="new-window-1"
           rel="noopener"
         >
-          helped drive the newly formed SRE Team.{" "}
+          Gitpod{" "}
           <img
             src="/images/main/external.svg"
             alt="opens in new window"
             className="external"
           />
         </a>{" "}
-      </p>
-      <p>
-        In January 2021,{" "}
-        <a
-          href="https://pawlean.com/2020/12/17/leaving-my-first-tech-job"
-          target="_blank"
-          aria-describedby="new-window-1"
-          rel="noopener"
-        >
-          I was ready for my next challenge!{" "}
-          <img
-            src="/images/main/external.svg"
-            alt="opens in new window"
-            className="external"
-          />
-        </a>{" "}
-        I joined Sky Betting & Gaming as a DevOps Engineer.
+        is an open source platform for automated and ready-to-code development
+        environments, enabling developers to describe their dev environment as
+        code and start instant and fresh development environments for each new
+        task directly from a browser.
       </p>
 
       <style jsx>
